225231f0db7ca8ea922c07765ce83820fd15e592
Rolando Casumpang <rcasumpang@gmail.com>
2020-01-05 21:42:45 +0800

    Added ES support

--- getRICoverage.py
+++ getRICoverage.py
@@ -17,48 +17,68 @@ from collections import namedtuple
 # reserved_fn         In ec2 it's describe_reserved_instances
 # reserved_listname   Attribute for the list of reserved instance. In ec2 it's 'ReservedInstances'
 # reserved_filters    Dictionary of attribute as keys and corresponding values to match. Could be empty
-# reserved_count      Attribute for the reserved instances count
+# reserved_count      Attribute for the reserved instances count. 
+# reserved_type       Attribute for the Reserved Instance type. If blank, 'instance_type' is used instead
 # dimensions          Attribute(s) used for additional dimensions appended to the instance_type key
 
 AWS_service = namedtuple('AWS_service',
     "describe_fn instances_listname instances_filters cluster_count  instance_type "
-    "reserved_fn reserved_listname  reserved_filters  reserved_count dimensions")
+    "reserved_fn reserved_listname  reserved_filters  reserved_count reserved_type dimensions")
 
 AWS_services_dict = {
-    # 'es' : AWS_service(
-    #     'describe_elasticsearch_domains','DomainStatusList',{}, 'NodeType', 'NumberOfNodes',
-    #     'describe_reserved_elasticsearch_instances','ReservedElasticsearchInstances',{},'NodeCount',''
-    #     ),
     'rds' : AWS_service(
         'describe_db_instances', 'DBInstances',{'DBInstanceStatus':'available'}, '','DBInstanceClass',
-        'describe_reserved_db_instances','ReservedDBInstances',{},'DBInstanceCount',
+        'describe_reserved_db_instances','ReservedDBInstances',{},'DBInstanceCount','',
         ['MultiAZ']
         ),
     'ec2' : AWS_service(
         'describe_instances', 'Reservations',{'State':{'Code': 16, 'Name': 'running'}},'','InstanceType', 
-        'describe_reserved_instances','ReservedInstances',{},'InstanceCount',
+        'describe_reserved_instances','ReservedInstances',{},'InstanceCount','',
         ''
         ),
     'elasticache' : AWS_service(
         'describe_cache_clusters', 'CacheClusters',{}, 'NumCacheNodes','CacheNodeType', 
-        'describe_reserved_cache_nodes','ReservedCacheNodes',{},'CacheNodeCount',
+        'describe_reserved_cache_nodes','ReservedCacheNodes',{},'CacheNodeCount','',
         ''
         ),
-
+    'es' : AWS_service(
+        'describe_elasticsearch_domains','DomainStatusList',{}, 
+           'ElasticsearchClusterConfig.InstanceCount', 'ElasticsearchClusterConfig.InstanceType',
+        'describe_reserved_elasticsearch_instances','ReservedElasticsearchInstances',{},
+           'ElasticsearchInstanceCount','ElasticsearchInstanceType',
+        []
+        ),
     'redshift' : AWS_service(
         'describe_clusters', 'Clusters',{}, 'NumberOfNodes', 'NodeType', 
-        'describe_reserved_nodes','ReservedNodes',{},'NodeCount',
+        'describe_reserved_nodes','ReservedNodes',{},'NodeCount','',
         ''
         ),
 }
+
+def flatten(d, parent_key='', sep='.'):
+    items = []
+    for k, v in d.items():
+        try:
+            items.extend(flatten(v, '%s%s%s' % (parent_key, k,sep)).items())
+        except AttributeError:
+            items.append(('%s%s' % (parent_key, k), v))
+    return dict(items)
+
 
 def getServiceInstances(service,regions,service_def):
     _instances = []
     svc=service_def[service]
     for region in regions:
-        client = boto3.client(service, region_name=region) 
-        service_client = getattr(client,svc.describe_fn)()
-        service_instances = service_client[svc.instances_listname]
+        client = boto3.client(service, region_name=region)
+        # Have to tell AWS to standardize their APIs and be consistent
+        service_instances=[]
+        if service == 'es':
+            domains = client.list_domain_names()['DomainNames']
+            for domain in domains:
+                service_instances.append(client.describe_elasticsearch_domain(DomainName=domain['DomainName'])['DomainStatus'])
+        else:
+            service_client = getattr(client,svc.describe_fn)()
+            service_instances = service_client[svc.instances_listname]
         # in ec2 instances are nested in Instances array
         if service == 'ec2':
             _groups_instances =[]
@@ -82,6 +102,7 @@ def getServiceReservedInstances(service,regions,service_def):
     for region in regions:
         client = boto3.client(service, region_name=region)
         # Filter by hand since "Filters=" is not available in all services.
+        service_client =[]
         service_client = getattr(client,svc.reserved_fn)()
         for reserved in service_client[svc.reserved_listname]:
              if reserved['State'] == 'active' :
@@ -103,8 +124,14 @@ def serviceReservedInstanceReportEx(service,regions,service_def):
         # merge structure: { 'm1.small': {'running':1,'reserved':5} }
         merged = {}
         
+        # flatten the structure to retrive the sub dict values
         for instance in [ i for i in instances if i['Region'] == region ]:
-            key = instance[svc.instance_type]
+            instance = flatten(instance)
+            is_reserved = ('State' in instance) and (instance['State'] == 'active')
+            if is_reserved and svc.reserved_type:
+               key = instance[svc.reserved_type]
+            else:
+               key = instance[svc.instance_type]
             if svc.dimensions:
                 key += ' (' 
                 for dim in svc.dimensions:
@@ -113,7 +140,7 @@ def serviceReservedInstanceReportEx(service,regions,service_def):
                 key += ')'
             if not key in merged :
                 merged[key] = {'running':0,'reserved':0}
-            if ( ('State' in instance) and (instance['State'] == 'active') ): 
+            if ( is_reserved ): 
                 merged[key]['reserved'] += instance[svc.reserved_count]
             else:
                 merged[key]['running'] += instance[svc.cluster_count] if svc.cluster_count else 1
@@ -143,7 +170,7 @@ def serviceReservedInstanceReportEx(service,regions,service_def):
 
 
 def main():
-    regions = ['us-east-1'] #,'us-west-2','eu-west-1','eu-central-1']
+    regions = ['us-east-1','us-west-2'] #,'us-west-2','eu-west-1','eu-central-1']
     for service in AWS_services_dict.keys():
         serviceReservedInstanceReportEx(service,regions,AWS_services_dict)
     
